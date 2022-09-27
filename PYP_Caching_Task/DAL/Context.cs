69b54a26a20e286c7375456488ce95c71ae82879
RasulRustamli <rasular@code.edu.az>
2022-09-27 04:58:51 +0400

    Add project files.

---
+++ PYP_Caching_Task/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PYP_Caching_Task.Models;
+
+namespace PYP_Caching_Task.DAL
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ PYP_Caching_Task/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PYP_Caching_Task.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
