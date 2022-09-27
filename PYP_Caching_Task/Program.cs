13c3f8d387f4fdb45166e0daecca5913bae9c6ff
RasulRustamli <rasular@code.edu.az>
2022-09-27 05:52:07 +0400

    refactor:update program cs

--- PYP_Caching_Task/Program.cs
+++ PYP_Caching_Task/Program.cs
@@ -7,8 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Context>(optopns => optopns.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddMemoryCache();
+builder.Services.AddDistributedSqlServerCache(options =>
+{
+    options.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.TableName = "CacheProduts";
+    options.SchemaName = "dbo";
+});
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
